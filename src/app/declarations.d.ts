564d610af764827bf50cccea437b3943431d8d91
Francesco Meli <francesco.meli@beefree.io>
2025-07-11 14:53:36 +0200

    fix: axios and clean envs

--- src/app/declarations.d.ts
+++ src/app/declarations.d.ts
@@ -1,12 +1,8 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      NEXT_PUBLIC_BASEPATH: string
-      NEXT_PUBLIC_BEEPLUGIN_URL: string
       NEXT_PUBLIC_BEEPLUGIN_AUTH_ENDPOINT: string
       NEXT_PUBLIC_BEEPLUGIN_AUTH_REFRESH: string
-      NEXT_PUBLIC_BEEFREE_ACCESS_TOKEN_NAME: string
-      NEXT_PUBLIC_BEEFREE_UID: string
       NEXT_PUBLIC_CONTENT_SERVICE_API: string
       BEEPLUGIN_AUTH_VERIFY: string
       BEEPLUGIN_AUTH_EMAIL_CLIENT_ID: string
@@ -18,4 +14,5 @@ declare global {
 
 // If this file has no import/export statements (i.e. is a script)
 // convert it into a module by adding an empty export statement.
-export {}
+// eslint-disable-next-line prettier/prettier
+export { }
--- src/helpers/axios.ts
+++ src/helpers/axios.ts
@@ -4,92 +4,53 @@ import axios, {
   AxiosResponse,
   InternalAxiosRequestConfig
 } from "axios"
-import mem from "mem"
 import { v4 as uuidv4 } from "uuid"
 
-// CustomAxios Interfaces are needed by TS to manage custom props on config
-interface CustomAxiosConfig extends InternalAxiosRequestConfig {
-  sent?: boolean
-}
-interface CustomAxiosError extends AxiosError {
-  config?: CustomAxiosConfig
-}
+const tokenName = "token"
 
 const clientAxiosInstance: AxiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASEPATH,
-  withCredentials: true,
   headers: { "Content-Type": "application/json" }
 })
 
-const loginV2Fn = async (template_type: string) =>
-  clientAxiosInstance.post("/api/auth/login", { template_type })
-const refreshTokenFn = async () => clientAxiosInstance.post("/api/auth/refresh")
-
-/**
- * Memorized functions prevents multiple parallel calls to bee-auth,
- * max age set to 10 secs to include all http request on the same page
- */
-const memorizedLoginV2 = mem(loginV2Fn, { maxAge: 10000 })
-const memorizedRefreshToken = mem(refreshTokenFn, { maxAge: 10000 })
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const bearerInterceptor = (config: any) => {
-  if (config.withCredentials === false) {
-    return config
-  }
-
-  const token = localStorage.getItem(
-    process.env.NEXT_PUBLIC_BEEFREE_ACCESS_TOKEN_NAME
-  )
-  const uid = localStorage.getItem(process.env.NEXT_PUBLIC_BEEFREE_UID)
-
-  if (!uid) {
-    localStorage.setItem(
-      process.env.NEXT_PUBLIC_BEEFREE_UID,
-      `${process.env.NEXT_PUBLIC_BEEFREE_UID_PREFIX}-${uuidv4()}`
-    )
-  }
-
-  return {
-    ...config,
-    headers: {
-      ...config.headers,
-      Authorization: `Bearer ${token}`,
-      uid
-    }
-  }
+const bearerInterceptor = (
+  config: InternalAxiosRequestConfig
+): InternalAxiosRequestConfig => {
+  const token = localStorage.getItem(tokenName)
+  config.headers.set("Authorization", `Bearer ${token}`)
+  config.headers.set("uid", uuidv4())
+  return config
 }
 
-const errorInterceptor = async (error: CustomAxiosError) => {
+const responseInterceptor = async (response: AxiosResponse) => {
+  const config = response?.config
+  if (config?.url?.match(/(\/refresh|\/login)/)) {
+    const { access_token } = response.data
+    if (access_token) {
+      localStorage.setItem(tokenName, access_token)
+    }
+  }
+  return response
+}
+
+const loginV2 = async (template_type: string) =>
+  clientAxiosInstance.post("/api/auth/login", { template_type })
+
+const refreshToken = async () => clientAxiosInstance.post("/api/auth/refresh")
+
+const errorInterceptor = async (error: AxiosError) => {
   const { config } = error
   if (config) {
     const template_type = config?.params?.get("template_type") ?? "email"
     let result
-    if (error?.response?.status === 401 && !config?.sent) {
-      config.sent = true
-      result = await memorizedRefreshToken()
-    } else if (error?.response?.status === 440 && !config.sent) {
-      config.sent = true
-      result = await memorizedLoginV2(template_type)
+    if (error?.response?.status === 401) {
+      result = await refreshToken()
+    } else if (error?.response?.status === 440) {
+      result = await loginV2(template_type)
     }
 
     return result ? clientAxiosInstance(config) : Promise.reject(error)
   }
   return Promise.reject(error)
-}
-
-const responseInterceptor = async (response: AxiosResponse) => {
-  const config = response?.config
-
-  // If the response comes from refresh or login calls, store the new token
-  if (config?.url?.match(/(\/refresh|\/login)/)) {
-    const { access_token } = response.data
-    const tokenName = process.env.NEXT_PUBLIC_BEEFREE_ACCESS_TOKEN_NAME
-    if (access_token && tokenName) {
-      localStorage.setItem(tokenName, access_token)
-    }
-  }
-  return response
 }
 
 clientAxiosInstance.interceptors.request.use(bearerInterceptor)
